054c52bd276cdc5eca2230dad43d9f821b71621c
joker234 <github@joker234.de>
2013-02-08 02:00:34 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kummerkasten::Application.config.secret_token = '6e399f58ee5b35e8027e47f0f0524dfdc393ed24170bbf124ec63c69d13a8d990bc3fce64981c3e6e7dc3db71015374c9a042fc2a35cf1d87112318cdcfdbd9b'
